7d065c6ab374b20e79997bdfda90fae86ce32b2d
wwl <243017037@qq.com>
2018-06-13 17:41:04 +0800

    spring boot 练习

--- SpringBootTry/src/main/java/com/example/SpringBootTry/controller/SpringController.java
+++ SpringBootTry/src/main/java/com/example/SpringBootTry/controller/SpringController.java
@@ -1,18 +1,30 @@
 package com.example.SpringBootTry.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SpringController {
 	
-	@RequestMapping(value="/aa")
+	@RequestMapping(value="/a")
 	public @ResponseBody String aa(){
+		System.out.println("aaaaaa");
 		return "fighting";
 	}
 	
-	@RequestMapping("/t")
-	public @ResponseBody String t(){
+	@RequestMapping(value="/b",method = RequestMethod.GET)
+	public ModelAndView index(){
+		System.out.println("bbbbbb");
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping(value="/c")
+	public @ResponseBody String c(){
+		System.out.println("ccccc");
 		return "Hello World!!";
 	}
+	
+
 }
